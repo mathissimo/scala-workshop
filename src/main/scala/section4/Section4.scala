282879ddd74573bbe3f91c3bba5db62a9dcfd2a7
Mathias Muenscher <mmsec@t101.de>
2016-11-10 15:48:47 +0100

    Finished Section4

--- src/main/scala/section4/Section4.scala
+++ src/main/scala/section4/Section4.scala
@@ -1,21 +1,34 @@
 package section4
 
+import scala.beans.{BeanProperty, BooleanBeanProperty}
+
 object Section4 {
 
   class Cigarettes
   case class UnderAgeException(message: String) extends Exception(message)
   case class NoIdException(message: String) extends Exception(message)
 
-  case class Customer(age: Int, haveId: Boolean = false) {
+  case class Customer(@BeanProperty age: Int, @BooleanBeanProperty haveId: Boolean = false) {
     // hint: you can use the require function
+    require(age > 0, s"customer have illegal age $age")
   }
 
   class Store {
-    def buyCigarettes(customer: Customer): Cigarettes = ???
+    def buyCigarettes(customer: Customer): Cigarettes = {
+      if (customer.age<18) throw new UnderAgeException("Customer must be older than 18 but was "+ customer.age)
+      if (!customer.haveId) throw new NoIdException("")
+        new Cigarettes
+    }
   }
 
   class LaxistStore extends Store {
-    override def buyCigarettes(customer: Customer): Cigarettes = ???
+    override def buyCigarettes(customer: Customer): Cigarettes = {
+      try {
+        super.buyCigarettes(customer)
+      } catch {
+        case exception: NoIdException => new Cigarettes
+      }
+    }
   }
 
 }
